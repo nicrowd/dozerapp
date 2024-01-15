f867b3317776fbe01f663a8e979c39958f1a3b46
nicrowd <serbond@gmail.com>
2024-01-15 22:53:15 +0000

    dozer app

---
+++ dozer_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import base64
+
+# Function to download data
+def get_table_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="drinkogniser_responses.csv">Download csv file</a>'
+    return href
+
+def main():
+    st.title("Drinkogniser")
+
+    with st.form("user_info_form"):
+        name = st.text_input("Enter your name")
+        email = st.text_input("Enter your email")
+        design_option = st.selectbox("Select your 'drinkorganiser' design", 
+                                     ["Design 1", "Design 2", "Design 3", "Design 4"])
+        selected_color = st.color_picker("Pick a color for the design", '#00f900')
+        
+        # Form submission
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            user_data = {
+                "Name": name,
+                "Email": email,
+                "Design": design_option,
+                "Color": selected_color
+            }
+            df = pd.DataFrame([user_data])
+            st.write(df)
+            st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
