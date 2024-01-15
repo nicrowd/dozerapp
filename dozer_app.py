754bba0c516d347ca36df4a8998db7a4aed70102
nicrowd <serbond@gmail.com>
2024-01-15 23:38:57 +0000

    dozer changed again

--- dozer_app.py
+++ dozer_app.py
@@ -6,7 +6,7 @@ import base64
 def get_table_download_link(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="drinkogniser_responses.csv">Download csv file</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="drinkogniser_responses.csv">Download and email us your choices as attached file</a>'
     return href
 
 def main():
