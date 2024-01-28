4f104e8dcf3cdf795a312f60cc7cafb9ce654086
nicrowd <156719172+nicrowd@users.noreply.github.com>
2024-01-28 17:01:10 +0000

    order app

---
+++ pages/02_Order_Customised_Design.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+import pandas as pd
+
+
+options = ['Generic', 'Girls', 'Boys', 'Mixed Gender', 'Pirate', 'Princess', 
+           'Dinosaur','Mermaid', 'Safari']
+
+themes = ["Kids Party", "Conference", "Family Party", 
+          "Christmas", "Halloween", "Special Launch Event",
+          ]
+
+pack_price = 3.99
+
+
+st.header("Customised Design Options")
